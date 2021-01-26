951e755eedf00e69f82b2b29ba9dbd224070a922
drmyr <drmyr@local>
2021-01-26 07:53:04 -0600

    longest substring without repeating characters

---
+++ src/main/java/general/LongestSubstringWihtoutRepeatingChars.java
@@ -0,0 +1,30 @@
+package general;
+
+public class LongestSubstringWihtoutRepeatingChars {
+
+    /*
+     https://leetcode.com/problems/longest-substring-without-repeating-characters
+     */
+    public static int lengthOfLongestSubstring(final String str) {
+        if(str.length() == 0) return 0;
+
+        final int[] map = new int[256];
+
+        int longest = 1;
+
+        int tail = 0;
+        int head = 0;
+        while(head < str.length()) {
+            if(map[str.charAt(head)] == 0) {
+                map[str.charAt(head)]++;
+                longest = Math.max(longest, (head-tail+1));
+                head++;
+            } else {
+                map[str.charAt(tail)]--;
+                tail++;
+            }
+        }
+
+        return longest;
+    }
+}
--- src/main/java/trees/JobScheduleMinDifficulty.java
+++ src/main/java/trees/JobScheduleMinDifficulty.java
@@ -10,9 +10,9 @@ public class JobScheduleMinDifficulty {
     /*
      https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
 
-     First attempt at the problem. Appears to be a valid solution, but too slow on large inputs, and times out.
+     First attempt at the problem. Appears to be a valid solution, but too slow on medium to large inputs, and times out.
 
-     Imperatively creates all possible divisions for the number of given days.
+     Imperatively creates all possible job divisions for the number of given days.
     */
     public static int minDifficultyTooSlow(final int[] jobDifficulty, final int days) {
         if(jobDifficulty.length < days) return -1;
@@ -35,7 +35,7 @@ public class JobScheduleMinDifficulty {
         // As an example, given a job schedule of {6,5,4,3,2,1} and days = 4, the initial range division would
         // be [[0,0,2], [1,1,3], [2,2,4], [3,5,5]], as the last day has to span the range of all remaining tasks.
         // The final range division would be [[0,2,2], [3,3,3], [4,4,4], [5,5,5]], as the first day has to span the
-        // range of all preceeding tasks.
+        // range of all preceding tasks.
         final int[][] ranges = new int[days][];
         for(int i = 0; i < ranges.length; i++) {
             if(i == (ranges.length - 1)) {
@@ -78,8 +78,10 @@ public class JobScheduleMinDifficulty {
                 return minDifficulty;
             } else {
 
-                // re-adjust the range spans, incrementally moving them towards the tail
-                // until all spans have been moved to their final day.
+                // Re-adjust the range spans, incrementally moving them towards the tail
+                // until all spans have been moved to their final day. This will generate all possible divisions
+                // of job tasks given the number of days. This is assuredly the part of the logic that needs
+                // to be optimized away by DP.
 
                 int currentRangeToModify = days - 2;
                 int[] currentRange;
@@ -112,4 +114,60 @@ public class JobScheduleMinDifficulty {
 
         return minDifficulty;
     }
+
+    /*
+      Not mine, just wanted the example handy.
+
+      https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/490316/JavaC++Python3-DP-O(nd)-Solution/811816
+     */
+//    public static int minDiff(int[] jobDifficulty, int D) {
+//        final int N = jobDifficulty.length;
+//        if(N < D) return -1;
+//        int[][] dp = new int[D][N];
+//
+//        dp[0][0] = jobDifficulty[0];
+//        for(int j = 1; j < N; ++j){
+//            dp[0][j] = Math.max(jobDifficulty[j], dp[0][j - 1]);
+//        }
+//
+//        for(int d = 1; d < D; ++d){
+//            for(int len = d; len < N; ++len){
+//                int localMax = jobDifficulty[len];
+//                dp[d][len] = Integer.MAX_VALUE;
+//                for(int schedule = len; schedule >= d; --schedule){
+//                    localMax = Math.max(localMax, jobDifficulty[schedule]);
+//                    dp[d][len] = Math.min(dp[d][len], dp[d - 1][schedule - 1] + localMax);
+//                }
+//            }
+//        }
+//
+//        return dp[D - 1][N - 1];
+//    }
+
+//    public int minDifficulty(int[] A, int D) {
+//        int n = A.length;
+//        if (n < D) return -1;
+//        int[] dp = new int[n], dp2 = new int[n], tmp;
+//        Arrays.fill(dp, 1000);
+//        Deque<Integer> stack = new ArrayDeque<Integer>();
+//
+//        for (int d = 0; d < D; ++d) {
+//            stack.clear();
+//            for (int i = d; i < n; i++) {
+//                dp2[i] = i > 0 ? dp[i - 1] + A[i] : A[i];
+//                while (!stack.isEmpty() && A[stack.peek()] <= A[i]) {
+//                    int j = stack.pop();
+//                    dp2[i] = Math.min(dp2[i], dp2[j] - A[j] + A[i]);
+//                }
+//                if (!stack.isEmpty()) {
+//                    dp2[i] = Math.min(dp2[i], dp2[stack.peek()]);
+//                }
+//                stack.push(i);
+//            }
+//            tmp = dp;
+//            dp = dp2;
+//            dp2 = tmp;
+//        }
+//        return dp[n - 1];
+//    }
 }
