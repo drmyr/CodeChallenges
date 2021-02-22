7ffc65d2ed64719a26ab05f4e97f9f47fed62919
drmyr <drmyr@local>
2021-02-22 07:50:36 -0600

    add comments

--- src/main/java/dfsbfs/FindArticulationPoints.java
+++ src/main/java/dfsbfs/FindArticulationPoints.java
@@ -9,6 +9,8 @@ import java.util.function.Predicate;
  * https://www.youtube.com/watch?v=2kREIkF9UAs
  * https://cp-algorithms.com/graph/cutpoints.html
  * https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/
+ * https://www.cs.cornell.edu/courses/cs2112/2012sp/lectures/lec24/lec24-12sp.html
+ * https://www.sanfoundry.com/java-program-tarjan-algorithm/
  *
  * Quoting CP-Algorithms (link above):
  *
@@ -25,67 +27,67 @@ public class FindArticulationPoints {
     public static <T> T[] criticalConnections(final T root, final T[][] edges) {
         final AtomicInteger ai = new AtomicInteger(0);
 
-        class LocalNode {
+        class MethodLocalNode {
             final T id;
             final Set<T> connections;
 
-            int rootChildrenCount = 0;
+            int rootChildCount = 0;
             boolean isRoot = false;
 
-            boolean countersSealed = false;
+            boolean countersInitialized = false;
             int disc = 0;
             int low = 0;
 
-            LocalNode(final T id) {
+            MethodLocalNode(final T id) {
                 this.id = id;
                 this.connections = new HashSet<>();
             }
 
             void setCounters() {
-                if(!this.countersSealed) {
+                if(!this.countersInitialized) {
                     final int next = ai.getAndIncrement();
                     this.disc = next;
                     this.low = next;
-                    this.countersSealed = true;
+                    this.countersInitialized = true;
                 }
             }
 
             /*
-             * If this node is a root node, we will only attempt to increment its `rootChildrenCount` more than once
+             * If this node is the root node, we will only attempt to increment its `rootChildrenCount` more than once
              * if it has two or more independent children (meaning this root node is an Articulation Point).
              * This is true because if this node is the root node, but not an Articulation Point, then in the
-             * `continueDFS` predicate below this root node never be able to contribute another child node that has
-             * not already been visited.
+             * `continueDFS` predicate below this root node never be able to contribute another child node to the
+             * DFS stack that has not already been visited.
              */
             void tryIncrementRootChildCount() {
                 if(isRoot) {
-                    rootChildrenCount++;
+                    rootChildCount++;
                 }
             }
 
         }
 
         // Undirected graph
-        final Map<T, LocalNode> graph = new HashMap<>();
+        final Map<T, MethodLocalNode> graph = new HashMap<>();
         for(final T[] edge : edges) {
-            final LocalNode alpha = graph.computeIfAbsent(edge[0], LocalNode::new);
-            final LocalNode beta = graph.computeIfAbsent(edge[1], LocalNode::new);
+            final MethodLocalNode alpha = graph.computeIfAbsent(edge[0], MethodLocalNode::new);
+            final MethodLocalNode beta = graph.computeIfAbsent(edge[1], MethodLocalNode::new);
             alpha.connections.add(beta.id);
             beta.connections.add(alpha.id);
         }
 
         final Set<T> visited = new HashSet<>();
         final Deque<T> stack = new ArrayDeque<>();
-        final LocalNode start = graph.get(root);
+        final MethodLocalNode start = graph.get(root);
         start.setCounters();
         start.isRoot = true;
         stack.push(start.id);
 
-        final Predicate<LocalNode> continueDFS = (final LocalNode node) -> {
+        final Predicate<MethodLocalNode> continueDFS = (final MethodLocalNode node) -> {
             visited.add(node.id);
             int low = node.low;
             for(final T id : node.connections) {
-                final LocalNode next = graph.get(id);
+                final MethodLocalNode next = graph.get(id);
                 if(!visited.contains(next.id)) {
                     next.setCounters();
                     stack.push(next.id);
@@ -100,7 +102,7 @@ public class FindArticulationPoints {
 
         final Set<T> articulationPoints = new HashSet<>();
 
-        LocalNode top;
+        MethodLocalNode top;
         while((top = graph.get(stack.peek())) != null) {
             if(!continueDFS.test(top)) {
                 /*
@@ -112,13 +114,13 @@ public class FindArticulationPoints {
                  two independent children for it to be an articulation point, else
 
                  2. the node needs to have a discovery time that is lower than at least one of its
-                 adjacent nodes.
+                 adjacent nodes, as a lower discovery time indicates that it is an articulation point.
 
                  */
-                final LocalNode node = graph.get(stack.pop());
+                final MethodLocalNode node = graph.get(stack.pop());
 
                 if(node.isRoot) {
-                    if(node.rootChildrenCount > 1) {
+                    if(node.rootChildCount > 1) {
                         articulationPoints.add(node.id);
                     }
                 } else {
