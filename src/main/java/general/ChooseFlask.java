d897f35b1ddcfbe65d43a176a893a2aaad3eff9a
drmyr <drmyr@local>
2021-02-26 20:34:24 -0600

    add description

--- src/main/java/general/ChooseFlask.java
+++ src/main/java/general/ChooseFlask.java
@@ -7,8 +7,29 @@ import java.util.TreeSet;
 
 public class ChooseFlask {
 
+    /**
+     * https://aonecode.com/amazon-online-assessment-choose-a-flask
+     *
+     *      "A robotic chemical delivery system for an Amazon laboratory has been configured to work using only
+     *       one type of glass flask per day. For each chemical ordered, it will be filled to a mark that is at
+     *       least equal to the volume ordered. There are multiple flasks available, each with markings at various levels.
+     *
+     *       Given a list of order requirements and a list of flasks with their measurements, write an algorithm
+     *       to determine the single type of flask that will result in minimal waste. Waste is the sum of
+     *       marking - requirement for each order. Output the zero-based index of the flask type chosen. If there are
+     *       multiple answers, output the minimum index. If no flask will satisfy the constraints, output -1."
+     *
+     * @param numOrders
+     * @param requirements
+     * @param flaskTypes
+     * @param totalMarkings
+     * @param markings
+     * @return
+     */
     public static int flaskFinder(final int numOrders, final int[] requirements, final int flaskTypes, final int totalMarkings, final int[][] markings) {
         Arrays.sort(requirements);
+
+        final int largest = requirements[requirements.length - 1];
 
         class Flask {
             final int id;
@@ -21,7 +42,7 @@ public class ChooseFlask {
             @SuppressWarnings("ConstantConditions")
             int calculateWaste() {
                 // If the flask is too small for the requirement, return early.
-                if(requirements[requirements.length - 1] > this.graduations.last()) {
+                if(largest > this.graduations.last()) {
                     return Integer.MAX_VALUE;
                 }
 
