465f082ff4b900b75cd29635c14c30d7209bc89c
drmyr <>
2022-01-11 08:19:22 -0500

    slight change

--- src/main/java/general/StringCompression.java
+++ src/main/java/general/StringCompression.java
@@ -1,7 +1,5 @@
 package general;
 
-import java.util.Arrays;
-
 public class StringCompression {
 
     /**
--- src/test/java/general/StringCompressionTest.java
+++ src/test/java/general/StringCompressionTest.java
@@ -10,8 +10,19 @@ class StringCompressionTest {
 
     @Test
     void compressTest() {
-        assertThat(compress(new Character[] {'a','a','b','c','c','c','b','b'}), Matchers.arrayContaining('a','2','b','c','3','b','2',' '));
-        assertThat(compress(new Character[] {'a','a','a','a','a','a','a','a','a','a','a','a'}), Matchers.arrayContaining('a','1','2',' ',' ',' ',' ',' ',' ',' ',' ',' '));
-        assertThat(compress(new Character[] {'a','b','b','c','b','b'}), Matchers.arrayContaining('a','b','2','c','b','2'));
+        final Character[] input1 = {'a','a','b','c','c','c','b','b'};
+        final Character[] output1 = compress(input1);
+        assertThat(output1, Matchers.arrayContaining('a','2','b','c','3','b','2',' '));
+        assertThat("objects must be the same", input1 == output1);
+
+        final Character[] input2 = {'a','a','a','a','a','a','a','a','a','a','a','a'};
+        final Character[] output2 = compress(input2);
+        assertThat(output2, Matchers.arrayContaining('a','1','2',' ',' ',' ',' ',' ',' ',' ',' ',' '));
+        assertThat("objects must be the same", input2 == output2);
+
+        final Character[] input3 = {'a','b','b','c','b','b'};
+        final Character[] output3 = compress(input3);
+        assertThat(output3, Matchers.arrayContaining('a','b','2','c','b','2'));
+        assertThat("objects must be the same", input3 == output3);
     }
 }
