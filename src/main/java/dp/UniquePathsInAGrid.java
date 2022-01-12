9b845e5415e3f6f72619eecc231e1306f43c71f4
drmyr <>
2022-01-12 11:56:55 -0500

    try again

--- src/main/java/dp/UniquePathsInAGrid.java
+++ src/main/java/dp/UniquePathsInAGrid.java
@@ -21,6 +21,27 @@ public class UniquePathsInAGrid {
      * @return
      */
     public static int uniquePathCount(final int[][] obstacleGrid) {
-        throw new UnsupportedOperationException("UniquePathsInAGrid.uniquePathCount");
+        if(obstacleGrid[0][0] == 1) return 0;
+        if(obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) return 0;
+
+        final int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+
+        for(int r = 0; r < obstacleGrid.length; r++) {
+            for(int c = 0; c < obstacleGrid[r].length; c++) {
+                if(r == 0 && c == 0) {
+                    dp[r][c] = 1;
+                } else if(r == 0) {
+                    dp[r][c] = (obstacleGrid[r][c] == 0) ? dp[r][c - 1] : dp[r][c];
+                } else if(c == 0) {
+                    dp[r][c] = (obstacleGrid[r][c] == 0) ? dp[r - 1][c] : dp[r][c];
+                } else {
+                    if(obstacleGrid[r][c] == 0) {
+                        dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+                    }
+                }
+            }
+        }
+
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
 }
