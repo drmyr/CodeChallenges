9a5cf5c07efe46ff424e3987462678eef379ab15
drm <drm@debian-BULLSEYE-live-builder-AMD64>
2022-01-25 09:50:51 -0500

    remove unnecessary refs

--- src/main/java/general/AddTwoNumbers.java
+++ src/main/java/general/AddTwoNumbers.java
@@ -20,27 +20,26 @@ public class AddTwoNumbers {
      * Explanation: 342 + 465 = 807.
      */
     public static ListNode addTwoNumbersLong(final ListNode l1, final ListNode l2) {
-         long l1sum = 0;
+         long sum = 0;
+
          int currentPower = 0;
          ListNode l1ref = l1;
          while(l1ref != null) {
-             l1sum += Math.pow(10, currentPower++) * l1ref.val;
+             sum += Math.pow(10, currentPower++) * l1ref.val;
              l1ref = l1ref.next;
          }
 
-         long l2sum = 0;
          currentPower = 0;
          ListNode l2ref = l2;
          while(l2ref != null) {
-             l2sum += Math.pow(10, currentPower++) * l2ref.val;
+             sum += Math.pow(10, currentPower++) * l2ref.val;
              l2ref = l2ref.next;
          }
 
-         long sum = (l2sum + l1sum);
          final ListNode root = new ListNode();
          ListNode next = root;
          while(sum != 0) {
-             next.val = (int)(sum % 10L);
+             next.val = (int)(sum % 10);
              sum /= 10;
              if(sum != 0) {
                  next.next = new ListNode();
@@ -55,23 +54,22 @@ public class AddTwoNumbers {
         final BigInteger ten = new BigInteger("10");
         final BigInteger zero = new BigInteger("0");
 
-        BigInteger l1sum = zero;
+        BigInteger sum = zero;
+
         int currPow = 0;
         ListNode l1ref = l1;
         while(l1ref != null) {
-            l1sum = l1sum.add(ten.pow(currPow++).multiply(BigInteger.valueOf(l1ref.val)));
+            sum = sum.add(ten.pow(currPow++).multiply(BigInteger.valueOf(l1ref.val)));
             l1ref = l1ref.next;
         }
 
-        BigInteger l2sum = zero;
         currPow = 0;
         ListNode l2ref = l2;
         while(l2ref != null) {
-            l2sum = l2sum.add(ten.pow(currPow++).multiply(BigInteger.valueOf(l2ref.val)));
+            sum = sum.add(ten.pow(currPow++).multiply(BigInteger.valueOf(l2ref.val)));
             l2ref = l2ref.next;
         }
 
-        BigInteger sum = l1sum.add(l2sum);
         final ListNode root = new ListNode();
         ListNode next = root;
         while(!sum.equals(zero)) {
