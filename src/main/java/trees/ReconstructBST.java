6e061d96f79b032fe6c986f7a48209ea1a4d9668
drm <drm@debian-BULLSEYE-live-builder-AMD64>
2022-04-21 09:48:12 -0400

    reconstruct bst from pre-order

--- src/main/java/trees/ReconstructBST.java
+++ src/main/java/trees/ReconstructBST.java
@@ -1,2 +1,38 @@
-package trees;public class ReconstructBST {
+package trees;
+
+import models.BinaryNodeMutable;
+
+import java.util.List;
+import java.util.TreeMap;
+
+import static java.util.Collections.reverseOrder;
+
+public class ReconstructBST {
+
+    /**
+     * Rebuild a BinaryNodeMutable given its pre-order traversal
+     */
+    public static BinaryNodeMutable reconstructBst(final List<Integer> preOrderTraversalValues) {
+        final BinaryNodeMutable root = new BinaryNodeMutable(preOrderTraversalValues.get(0));
+
+        final TreeMap<Integer, BinaryNodeMutable> map = new TreeMap<>(reverseOrder());
+        map.put(root.getValue(), root);
+
+        BinaryNodeMutable current = root;
+        for(int index = 1; index < preOrderTraversalValues.size(); index++) {
+            final int value = preOrderTraversalValues.get(index);
+            if(value < current.getValue()) {
+                current.setLeft(new BinaryNodeMutable(value));
+                current = current.getLeft();
+            } else {
+                // get if it already exists in the case of BST's that allow duplicates
+                final BinaryNodeMutable parent = map.getOrDefault(value, map.higherEntry(value).getValue());
+                parent.setRight(new BinaryNodeMutable(value));
+                current = parent.getRight();
+            }
+            map.put(value, current);
+        }
+
+        return root;
+    }
 }
--- src/test/java/dfsbfs/RiverSizesTest.java
+++ src/test/java/dfsbfs/RiverSizesTest.java
@@ -2,10 +2,28 @@ package dfsbfs;
 
 import org.junit.jupiter.api.Test;
 
+import static dfsbfs.RiverSizes.riverSizes;
 import static org.junit.jupiter.api.Assertions.*;
+
 class RiverSizesTest {
 
     @Test
-    void riverSizes() {
+    void riverSizesTest() {
+        int[][] matrix = {
+          {1, 1, 0},
+          {1, 0, 1},
+          {1, 1, 1},
+          {1, 1, 0},
+          {1, 0, 1},
+          {0, 1, 0},
+          {1, 0, 0},
+          {1, 0, 0},
+          {0, 0, 0},
+          {1, 0, 0},
+          {1, 0, 1},
+          {1, 1, 1}
+        };
+
+        riverSizes(matrix);
     }
 }
