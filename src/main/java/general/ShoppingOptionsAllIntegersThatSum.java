77c2f518acc2eae609c7c9b8e784a7fb8894b0ac
drm <drm@debian-BULLSEYE-live-builder-AMD64>
2022-07-23 19:16:43 -0400

    print all numbers that sum to target number

---
+++ src/main/java/general/ShoppingOptionsAllIntegersThatSum.java
@@ -0,0 +1,31 @@
+package general;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ShoppingOptionsAllIntegersThatSum {
+
+    public static List<List<Integer>> getAllIntegersThatSumToTarget(final int target) {
+        final int stoppingPoint = target / 2;
+        final List<List<Integer>> result = new ArrayList<>();
+        rec(result, 0, target, 1);
+        for(var entry : result) {
+            System.out.println(Arrays.toString(entry.toArray()));
+        }
+        return result;
+    }
+
+    public static void rec(final List<List<Integer>> result, final int current, final int target, final int step) {
+        if(current < target - step) {
+            final List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < current; i += step) list.add(step);
+            list.add(step);
+            list.add(target - step - current);
+            result.add(list);
+            rec(result, current + step, target, step);
+        } else if(step < (target / 2)) {
+            rec(result, 0, target, step + 1);
+        }
+    }
+}
