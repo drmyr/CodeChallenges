85e3e67897696a183cb96c8233cd8c90ea98b70b
drm <drm@debian-BULLSEYE-live-builder-AMD64>
2022-07-23 20:56:17 -0400

    add description

--- src/main/java/general/ShoppingOptionsAllIntegersThatSum.java
+++ src/main/java/general/ShoppingOptionsAllIntegersThatSum.java
@@ -6,6 +6,29 @@ import java.util.List;
 
 public class ShoppingOptionsAllIntegersThatSum {
 
+    /**
+     * https://cybergeeksquad.co/2021/06/shopping-options-amazon-online-assessment.html
+     *
+     * Shopping Options Amazon Online Assessment
+     *
+     * Alternative Question: Find All Combination of Numbers Sum to Target
+     *
+     * Given a positive integer, target, print all possible combinations of positive integers that sum up to the target number.
+     *
+     * For example, if we are given input ‘5’, these are the possible sum combinations.
+     *
+     * 1, 4
+     * 2, 3
+     * 1, 1, 3
+     * 1, 2, 2
+     * 1, 1, 1, 2
+     * 1, 1, 1, 1, 1
+     *
+     * The output will be in the form a list of lists or an array of arrays. Each element in the list will be another list containing a possible sum combination.
+     * 
+     * @param target
+     * @return
+     */
     public static List<List<Integer>> getAllIntegersThatSumToTarget(final int target) {
         final List<List<Integer>> result = new ArrayList<>();
         rec(result, 0, target, 1);
